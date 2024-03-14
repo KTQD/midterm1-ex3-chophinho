d8f37924fab4e3c62dde44d2eecf0ab072e3894a
Ngo Ha Phuong <121754520+chophinho@users.noreply.github.com>
2024-03-14 08:13:20 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            reader.close();
 
-        Bài làm viết tại đây
+            String replacedContent = content.toString().replaceAll("Nha Trang", "Vũng Tàu");
 
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write(replacedContent);
+            writer.close();
+
+            System.out.println("Đã thay thế và ghi nội dung vào tệp output.txt " );
+        } catch (IOException e) {
+            System.out.println("Lỗi: " + e.getMessage());
+        }
     }
 }
+
